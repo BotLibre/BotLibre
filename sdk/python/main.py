23453d63d0b4517a315c58e2ef4204447ffff356
Paphus Solutions Development Team <57759149+OmarBajunaid@users.noreply.github.com>
2023-09-07 12:01:18 -0400

    Fixed a bug and added gitignore
    Fixed a bug when trying one of the functions without entering username, password and applicationId. Now it throws an error message.
    Added .gitignore to ignore all the paycahse folders

--- sdk/python/main.py
+++ sdk/python/main.py
@@ -57,8 +57,8 @@ class Main(object):
         userConfig.application = self.applicationId
         userConfig.user =self.username
         userConfig.password = self.password
-        if(userConfig.user =="" or userConfig.password == "" or userConfig.application == ""):
-            Utils.log("Please fill the required data @ connectUserAccount in main.py")
+        if(self.username =="" or self.password == "" or self.application == ""):
+            Utils.log("[Main]","Please fill the required data @ connectUserAccount in main.py")
             return False
         self.connection.connect(userConfig)
         return True
